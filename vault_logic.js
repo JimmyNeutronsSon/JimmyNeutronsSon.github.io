fbd5d484214ba99dfaf9a4b9a66f6c51681fe4ff
JimmyNeutronsSon <ihtiram4396@gmail.com>
2026-05-04 15:06:22 -0700

    Change VAULT_PATH to local vault directory

--- vault_logic.js
+++ vault_logic.js
@@ -2,7 +2,7 @@
  * Welkin Vault Logic - V3 Refined (Scoped Classes)
  */
 
-const VAULT_PATH = 'https://raw.githack.com/WanoCapy/ChickenKingsVault/main/';
+const VAULT_PATH = 'vault/';
 let heroRotationIndex = 0;
 let heroGames = [];
 
